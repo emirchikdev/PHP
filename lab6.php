497384733ff75435d4c856af3dd51ae9bea2a1cb
Ysak-Emir <emirchik.isak@gmail.com>
2026-03-21 21:20:20 +0600

    lab 6 update

--- lab6.php
+++ lab6.php
@@ -2,6 +2,13 @@
 // Лабаратория №6
 
 //1
+function str_count($str, $substr): int {
+    return substr_count($str, $substr);
+}
+
+echo str_count('hello', 'l'); 
+
+//2
 function max_number(int $num): int {
     $digits = str_split((string)$num);
     rsort($digits);
@@ -10,7 +17,7 @@ function max_number(int $num): int {
 
 echo max_number(123);
 
-//2
+//3
 function no_space(string $str): string {
     return str_replace(' ', '', $str);
 }
